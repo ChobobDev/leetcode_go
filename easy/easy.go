cb6516a47ac0697dedcb5122bf3a646a4cab522a
chobobdev <chobobdev@naver.com>
2022-03-01 22:34:50 +0900

    Solved: Easy - 26

--- easy/easy.go
+++ easy/easy.go
@@ -3,5 +3,5 @@ package easy
 import "fmt"
 
 func Easy() {
-	fmt.Println(longestCommonPrefix([]string{"this", "that", "them", "they"}))
+	fmt.Println(removeDuplicates([]int{1, 1, 2, 3, 2, 3, 4, 5, 6, 7, 8, 6, 9}))
 }
